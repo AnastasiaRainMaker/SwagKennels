54d967d6d7785f9281d9f16c384c5561d8d8ee3a
Anastasia <humaine@yandex.ru>
2018-05-16 09:38:13 -0500

    third commit

---
+++ app/src/main/java/com/project/swagkennels/adapters/DogsFragmentAdapter.java
@@ -0,0 +1,102 @@
+package com.project.swagkennels.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.project.swagkennels.Dog;
+import com.project.swagkennels.R;
+
+
+import java.util.ArrayList;
+
+public class DogsFragmentAdapter extends RecyclerView.Adapter<DogsFragmentAdapter.ViewHolder> {
+    private ArrayList<Dog> dataList;
+    private Context context;
+
+    public DogsFragmentAdapter(ArrayList<Dog> data, Context context) {
+        this.dataList = data;
+        this.context = context;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textViewDescription;
+        TextView titleView;
+        CardView card;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.image);
+            this.textViewDescription = itemView.findViewById(R.id.description);
+            this.titleView = itemView.findViewById(R.id.title);
+            this.card = itemView.findViewById(R.id.card);
+        }
+    }
+
+    @NonNull
+    @Override
+    public DogsFragmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.dogs_recyclerview_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final DogsFragmentAdapter.ViewHolder holder, final int position) {
+        final Dog item = dataList.get(position);
+        if(item != null) {
+
+//            holder.card.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent intent = new Intent(context, NewsItemActivity.class);
+//
+//                  //  intent.putExtra("imageUrl", "");
+//                    intent.putExtra("description", item.getDescription());
+//                    intent.putExtra("title", item.getTitle());
+//                    intent.putExtra("imgUrl", item.getImageUrl());
+//
+//                    String transitionName = context.getString(R.string.transition_shared_element);
+//
+//                    ActivityOptionsCompat options =
+//                            ActivityOptionsCompat.makeSceneTransitionAnimation((NewsListActivity) context,
+//                                    holder.imageView,   // Starting view
+//                                    transitionName    // The String
+//                            );
+//                    ActivityCompat.startActivity(context, intent, options.toBundle());
+//                }
+//            });
+
+            if(item.getImageUrl() == null) {
+                holder.imageView.setBackgroundResource(R.drawable.milady);
+            } else {
+                //use link to load pic into view
+            }
+            if (item.getTitle() == null) {
+                holder.titleView.setVisibility(View.GONE);
+            } else {
+                holder.titleView.setText(item.getTitle());
+            }
+            if(item.getDescription() != null) {
+                holder.textViewDescription.setText(item.getDescription());
+            } else {
+                holder.textViewDescription.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
+
--- app/src/main/java/com/project/swagkennels/fragments/DogsFragment.java
+++ app/src/main/java/com/project/swagkennels/fragments/DogsFragment.java
@@ -3,18 +3,57 @@ package com.project.swagkennels.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.project.swagkennels.Dog;
+import com.project.swagkennels.News;
 import com.project.swagkennels.R;
+import com.project.swagkennels.adapters.DogsFragmentAdapter;
+import com.project.swagkennels.adapters.NewsFragmentAdapter;
+
+import java.util.ArrayList;
 
 public class DogsFragment extends Fragment {
+
+    ArrayList<Dog> dogList;
+    RecyclerView recyclerView;
+    DogsFragmentAdapter adapter;
+    ProgressBar progressBar;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dogs, container, false);
+        View view = inflater.inflate(R.layout.fragment_dogs, container, false);
+        dogList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        dogList.add(new Dog(null, "Milady","Our beautiful dog!"));
+        dogList.add(new Dog(null, "Heize","Our beautiful dog!"));
+        dogList.add(new Dog(null, "Khalissi","Our beautiful dog!"));
+        dogList.add(new Dog(null, "Swaggy","Our beautiful dog!"));
+
+
+        adapter = new DogsFragmentAdapter(dogList, getContext());
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    public void showProgress(Boolean value) {
+        if (value && progressBar != null) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        if (!value && progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
